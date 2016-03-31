4556adaa1a710e69994abe45977d272443ed52c7
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-03-31 22:40:33 +0200

    Adds the first try

--- Example/Noise/Noise/Sources/ViewController.swift
+++ Example/Noise/Noise/Sources/ViewController.swift
@@ -7,6 +7,15 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     view.backgroundColor = UIColor.whiteColor()
+
+    let totalSize = UIScreen.mainScreen().bounds
+    let orange = UIView()
+    orange.frame = CGRect(x: (totalSize.width - 150) / 2,
+                          y: (totalSize.height - 300) / 2,
+                          width: 150, height: 150)
+    orange.layer.cornerRadius = 5
+    orange.backgroundColor = UIColor.orangeColor()
+
+    view.addSubview(orange)
   }
 }
-
