7d2108e79e94a3f1690f30b2eedd40e789fc2ab8
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-02 17:00:57 +0200

    Adds a better example

--- Example/Noise/Noise/Sources/ViewController.swift
+++ Example/Noise/Noise/Sources/ViewController.swift
@@ -9,26 +9,14 @@ class ViewController: UIViewController {
     view.backgroundColor = UIColor.whiteColor()
 
     let totalSize = UIScreen.mainScreen().bounds
-    let orange = NoiseView()
+    let orange = UIView()
     orange.frame = CGRect(x: (totalSize.width - 150) / 2,
                           y: (totalSize.height - 300) / 2,
                           width: 150, height: 150)
     orange.layer.cornerRadius = 5
     orange.backgroundColor = UIColor.orangeColor()
+    orange.noise(alpha: 0.4)
 
     view.addSubview(orange)
-  }
-}
-
-class NoiseView: UIView {
-
-  override func drawRect(rect: CGRect) {
-    super.drawRect(rect)
-
-    let context = UIGraphicsGetCurrentContext()
-    backgroundColor?.setFill()
-    CGContextFillRect(context, bounds);
-
-    buildNoise()
   }
 }
--- Sources/Noise.swift
+++ Sources/Noise.swift
@@ -1,32 +1,36 @@
 import UIKit
 
-func noise() {
+public func noise(view: UIView, alpha: CGFloat = 0.2, blend: CGBlendMode = .Overlay) {
 
-}
-
-struct Noise {
-
+  Noise.background(view, alpha, blend)
 }
 
 extension UIView {
 
-  public func noise() {
-    let frame = CGRect(origin: CGPointZero, size: CGSize(width: bounds.width, height: bounds.height))
+  public func noise(alpha alpha: CGFloat = 0.2, _ blend: CGBlendMode = .Overlay) {
+    Noise.background(self, alpha, blend)
+  }
+}
+
+struct Noise {
+
+  static func background(view: UIView, _ alpha: CGFloat = 0.2, _ blend: CGBlendMode = .Overlay) {
+    let frame = view.bounds
     UIGraphicsBeginImageContextWithOptions(frame.size, true, 0)
 
     let context = UIGraphicsGetCurrentContext()
 
-    backgroundColor?.setFill()
+    view.backgroundColor?.setFill()
     CGContextFillRect(context, frame)
-    buildNoise()
+    buildNoise(alpha, blend)
 
     let image = UIGraphicsGetImageFromCurrentImageContext()
     UIGraphicsEndImageContext()
 
-    backgroundColor = UIColor(patternImage: image)
+    view.backgroundColor = UIColor(patternImage: image)
   }
 
-  public func buildNoise() {
+  static func buildNoise(alpha: CGFloat, _ blend: CGBlendMode = .Overlay) {
     struct Static {
       static var token: dispatch_once_t = 0
     }
@@ -36,13 +40,11 @@ extension UIView {
     dispatch_once(&Static.token) {
       let width = 128
       let size = width * width
-      let bitmapData = UnsafeMutablePointer<CUnsignedChar>(malloc(size))
+      let bitmapData = UnsafeMutablePointer<CChar>(malloc(size))
 
-      srand(124)
-
-      for i in 0 ..< size - 1 {
-        let value = Int8(arc4random_uniform(128) % 256)
-        bitmapData[i] = CUnsignedChar(bitPattern: Int8(value))
+      for i in 0 ..< size {
+        let value = arc4random_uniform(128) % 256
+        bitmapData[i] = CChar(bitPattern: UInt8(value))
       }
 
       let colorSpace = CGColorSpaceCreateDeviceGray()
@@ -57,7 +59,7 @@ extension UIView {
     let context = UIGraphicsGetCurrentContext()
 
     CGContextSaveGState(context)
-    CGContextSetAlpha(context, 0.6)
+    CGContextSetAlpha(context, 0.2)
     CGContextSetBlendMode(context, .Overlay)
 
     let frame = CGRect(x: 0, y: 0,
